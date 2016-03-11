95d5d759d4f635efc5cd72c769fc284d26bf7b57
Ryan Purdy <Ryanpurdy@Ryans-MacBook-Pro-2.local>
2016-03-11 00:12:40 -0500

    Began entering variable info

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 // Create your variable here, name it `insects`
 var insects = [
   {
+    name: 'Ant Nest Beetle',
+    img: 'ant-nest-beetle.jpg',
+    family: 'Carabidae',
+    size: ['6', '20'],
 
 // ##########################################################
 // START DO NOT CHANGE ZONE
